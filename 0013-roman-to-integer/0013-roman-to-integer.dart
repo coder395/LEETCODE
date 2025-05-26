93c00f349bdc15b752c82428949668470db12b19
Hari <23MH1A0456@acoe.edu.in>
2025-05-26 19:38:08 +0530

    Time: 2 ms (99.49%), Space: 151.2 MB (36.64%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -0,0 +1,35 @@
+class Solution {
+  int romanToInt(String s) {
+    int result = 0;
+    int ans =0;
+    for (int i = s.length -1;i>=0;i--){
+        switch(s[i]){
+            case 'I':
+                result  = 1;
+                break;
+            case 'V':
+                result =5;
+                break;
+            case 'X':
+                result =10;
+                break;
+            case 'L':
+                result =50;
+                break;
+            case 'C':
+                result =100;
+                break;
+            case 'D':
+                result =500;
+                break;
+            case 'M':
+                result =1000;
+                break;
+        }
+        if(4*result < ans) ans-= result;
+        else ans += result;
+    }
+    return ans;
+    
+  }
+}
